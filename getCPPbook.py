b90cb2db05bfa1a45e754a030b771bb74779ff6b
JamesSadler-Dev <jamesironmanjames@gmail.com>
2024-02-25 15:50:14 -0600

    fixed bugs with css

--- getCPPbook.py
+++ getCPPbook.py
@@ -1,48 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def main():
 
-    months = {
-              "JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY"
-              "AUGUST", "SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER" 
-             }
-
     index = requests.get("https://www.learncpp.com").text
     text = BeautifulSoup(index,features="html.parser").select(".lessontable-row-title > a")
-
+    
     links= []
-    with open("cppbook.md","w",encoding="utf-8") as file:
-        file.write("<h1>LearnCPP.com to Book conversion</h1>\n\n<h2>Index:</h2>\n\n<ul>")
+    with open("cppbook.html","w",encoding="utf-8") as file:
+        file.write("""
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style.css">
+        </head>
+                   """)
+        file.write("<body><h1>LearnCPP.com to Book conversion</h1>\n\n<h2>Index:</h2>\n\n<ul>")
         
         for item in text:
             item_text = item.get_text()
             link = item.attrs.get("href","na")
 
             if link != "na":
-                links.append({item_text:link})
+                links.append(link)
                 file.write(f"<li><a href={link}><big>{item_text}</big></li><br>\n")
         file.write("</ul></a>")
 
-        for dictionary in links:
-            for item_text,link in dictionary.items():
-                page = requests.get(link).text
-                parsed = BeautifulSoup(page,features="html.parser").find("article").children
-                file.write(f"<h2>{item_text}</h2>\n")
+        for link in links:
+            page = requests.get(link).text
+            parsed = BeautifulSoup(page,features="html.parser").find_all("article")
+            
 
-                for child in parsed:
-                    child_text = child.get_text().splitlines()
-                    
-                    for line in child_text:
-
-                        if line.split(" ")[0].upper().strip() in months or len(line.split(" ")) == 1:
-                            continue
-                        elif line.upper() == "NEXT LESSON":
-                            break
-                        else:
-                            file.write(f"{line}\n")
-                    file.write("\n")
+            for article in parsed:
+                article = str(article).split("\n")
                 
+                for line in article:    
+
+                    line = line.strip()
+
+                    if len(line) > 1:
+                        file.write(f"{line}\n")
+        file.write("</body>")
+                
+
 
 main()
 
