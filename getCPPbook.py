6cd34842c37fdee71a655de7f2649f649f79cac2
James Sadler <jamesironmanjames@gmail.com>
2024-03-21 19:10:31 -0500

    added sections to go back to and stripped links

--- getCPPbook.py
+++ getCPPbook.py
@@ -26,20 +26,27 @@ def main():
                 file.write(f"<li><a href={link}><big>{item_text}</big></li><br>\n")
         file.write("</ul></a>")
 
+        i = 1
         for link in links:
             page = requests.get(link).text
             parsed = BeautifulSoup(page,features="html.parser").find_all("article")
             
-
+            
             for article in parsed:
                 article = str(article).split("\n")
-                
+                section_code= f"<hr><a href=cppbook.html#sect{i} name=sect{i} class=sect>Section {i}</a>"
+                file.write(section_code)
+                i+=1
                 for line in article:    
 
+                    if line.startswith("<div class=\"prevnext"):
+                        break
                     line = line.strip()
-
+                    line = re.sub("</?a.+>","",line)
                     if len(line) > 1:
                         file.write(f"{line}\n")
+                    
+
         file.write("</body>")
                 
 
