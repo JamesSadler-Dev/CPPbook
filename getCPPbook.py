eb7012c8209322138806d5dc59975b58880b605a
James Sadler <jamesironmanjames@gmail.com>
2024-03-31 02:36:33 -0500

    add styling to cpp notes, remove a level of nesting in final loop by only getting one article tag each page

--- getCPPbook.py
+++ getCPPbook.py
@@ -29,24 +29,22 @@ def main():
         i = 1
         for link in links:
             page = requests.get(link).text
-            parsed = BeautifulSoup(page,features="html.parser").find_all("article")
+            parsed = BeautifulSoup(page,features="html.parser").find("article")
             
-            
-            for article in parsed:
-                article = str(article).split("\n")
-                section_code= f"<hr><a href=cppbook.html#sect{i} name=sect{i} class=sect>Section {i}</a>"
-                file.write(section_code)
-                i+=1
-                for line in article:    
+            article = str(parsed).split("\n")
+            section_code= f"<hr><a href=cppbook.html#sect{i} name=sect{i} class=sect>Section {i}</a>"
+            file.write(section_code)
+            i+=1
+            for line in article:    
 
-                    if line.startswith("<div class=\"prevnext"):
-                        break
-                    line = line.strip()
-                    if line.startswith("<a"):
-                        line = re.sub("</?a.+>","",line)
-                    if len(line) > 1:
-                        file.write(f"{line}\n")
-                    
+                if line.startswith("<div class=\"prevnext"):
+                    break
+                line = line.strip()
+                if line.startswith("<a"):
+                    line = re.sub("</?a.+>","",line)
+
+                file.write(f"{line}\n")
+                
 
         file.write("</body>")
                 
