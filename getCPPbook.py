6e8d6a0b9bb26cedac5b8f72d06bff46bde19204
James Sadler <jamesironmanjames@gmail.com>
2024-03-31 04:04:51 -0500

    Set some groundwork for pages update

--- getCPPbook.py
+++ getCPPbook.py
@@ -2,20 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def main():
-
-    index = requests.get("https://www.learncpp.com").text
-    text = BeautifulSoup(index,features="html.parser").select(".lessontable-row-title > a")
-    
-    links= []
-    with open("cppbook.html","w",encoding="utf-8") as file:
-        file.write("""
+def write_header(file):
+            file.write("""
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link rel="stylesheet" href="style.css">
         </head>
+        <body>
                    """)
-        file.write("<body><h1>LearnCPP.com to Book conversion</h1>\n\n<h2>Index:</h2>\n\n<ul>")
+            
+def main():
+    index = requests.get("https://www.learncpp.com").text
+    text = BeautifulSoup(index,features="html.parser").select(".lessontable-row-title > a")
+    page = 1
+    links= []
+    current_link = f"cppbook-pg{page}.html"
+    with open(current_link,"w",encoding="utf-8") as file:
+        write_header(file)
+        file.write("<h1>LearnCPP.com to Book conversion</h1>\n\n<h2>Table of Contents:</h2>\n\n<ul>")
         
         for item in text:
             item_text = item.get_text()
@@ -32,7 +36,7 @@ def main():
             parsed = BeautifulSoup(page,features="html.parser").find("article")
             
             article = str(parsed).split("\n")
-            section_code= f"<hr><a href=cppbook.html#sect{i} name=sect{i} class=sect>Section {i}</a>"
+            section_code= f"<hr><a href={current_link}#sect{i} name=sect{i} class=sect>Section {i}</a>"
             file.write(section_code)
             i+=1
             for line in article:    
