4b2ef5b98ed39ba25764806da7b7b95f10946988
James Sadler <jamesironmanjames@gmail.com>
2024-05-30 22:54:03 -0500

    get logic prepared to use pure functions

--- getCPPbook.py
+++ getCPPbook.py
@@ -37,15 +37,12 @@ def check_fix_and_write_lines(article,file):
         
 
 def main():
+    print("running...")
     index = requests.get("https://www.learncpp.com").text
     text = BeautifulSoup(index,features="html.parser").select(".lessontable-row-title > a")
     page_num = 1
     links= []
-    current_link = f"cppbook-pg{page_num}.html"
-    with open(current_link,"w",encoding="utf-8") as file:
-        write_head_tag(file)
-        write_header(file,page_num)
-    
+
     with open("tableofcontents.html","w",encoding="utf-8") as file:
         write_head_tag(file)
         file.write("<h1>CPP Book Table of contents </h1><a href=\"./cppbook-pg1.html\">To Book</a><hr><ul>")
@@ -58,47 +55,48 @@ def main():
                 file.write(f"<li><a href={link}><big>{item_text}</big></li><br>\n")
         file.write("</ul></a></body>")
 
-        file = open(current_link,"a",encoding="utf-8")
+    current_link = f"cppbook-pg{page_num}.html"
+    with open(current_link,"w",encoding="utf-8") as file:
+        write_head_tag(file)
+        write_header(file,page_num)
 
-        i = 1
-        for link in links:
-            page = requests.get(link).text
-            parsed = BeautifulSoup(page,features="html.parser").find("article")
-            
-            article = str(parsed).split("\n")
-            section_code= f"<hr><a href={current_link}#sect{i} name=sect{i} class=sect>Section {i}</a>"
-            file.write(section_code)
-            i+=1
-            
-            check_fix_and_write_lines(article,file)
+    file = open(current_link,"a",encoding="utf-8")
+    i = 1
+    for link in links:
+        page = requests.get(link).text
+        parsed = BeautifulSoup(page,features="html.parser").find("article")
+        article = str(parsed).split("\n")
+        section_code= f"<hr><a href={current_link}#sect{i} name=sect{i} class=sect>Section {i}</a>"
+        file.write(section_code)
+        i+=1
+        
+        check_fix_and_write_lines(article,file)
 
-            if i % 100 == 0:
-                page_num+=1
-                current_link= f"cppbook-pg{page_num}.html"
-                if page_num == 2:
+        if i % 50 == 0:
+            page_num+=1
+            current_link= f"cppbook-pg{page_num}.html"
+            if page_num == 2:
+                file.write(f"""
+                    <hr>
+                    <div class=footer>
+                        <a href={current_link}> <h2>Next Page</h2> </a>
+                    </div>
+                    """)
+            else:
+                    prev_link = f"cppbook-pg{page_num - 2}.html"
                     file.write(f"""
-                        <hr>
-                        <div class=footer>
-                            <a href={current_link}> <h2>Next Page</h2> </a>
-                        </div>
-                        """)
-                else:
-                      prev_link = f"cppbook-pg{page_num - 1}.html"
-                      file.write(f"""
-                        <hr>
-                        <div class=footer>
-                            <a href={prev_link}><h2>Previous Page</h2></a>
-                            <a href={current_link}><h2>Next Page</h2></a>
-                        </div>
-                        """)
-                      
-                write_closing_tags(file)
-                file = open(current_link,"w",encoding="utf-8")
-                write_head_tag(file)
-                write_header(file,page_num)
+                    <hr>
+                    <div class=footer>
+                        <a href={prev_link}><h2>Previous Page</h2></a>
+                        <a href={current_link}><h2>Next Page</h2></a>
+                    </div>
+                    """)            
+            write_closing_tags(file)
+            file = open(current_link,"w",encoding="utf-8")
+            write_head_tag(file)
+            write_header(file,page_num)
 
-
-        write_closing_tags(file)
+    write_closing_tags(file)
                 
         
                 
